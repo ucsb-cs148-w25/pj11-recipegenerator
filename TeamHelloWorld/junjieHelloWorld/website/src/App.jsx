48e81ca98a7e1703db1287b0cfaeaeccfb9663c9
Junjie Liu <junjieliu@ucsb.edu>
2025-01-17 23:07:39 -0800

    thisHelloWorldShouldWorkNow

---
+++ TeamHelloWorld/junjieHelloWorld/website/src/App.jsx
@@ -0,0 +1,12 @@
+import InputBox from './inputBox.jsx'
+
+function App() {
+    return (
+      <>
+        <h1 className="rainbow-text">Hello World! UwU</h1>
+        <InputBox></InputBox>
+      </>
+  );
+}
+
+export default App
